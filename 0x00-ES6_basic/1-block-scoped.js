8d131b55cb5ebd501b3ff20bae5431ab6ca86c81
Great <jgreat7702gmail.com>
2024-10-04 14:52:01 +0100

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Use let for block scope
+  let task = false;// Use let for block scope
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;   // Block-scoped declaration
-    let task2 = false; // Block-scoped declaration
+    let task = true;// Block-scoped declaration
+    let task2 = false;// Block-scoped declaration
   }
 
-  return [task, task2]; // Returns the values outside the if block
+  return [task, task2];// Returns the values outside the if block
 }
