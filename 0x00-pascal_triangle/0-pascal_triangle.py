1951e4916f2db7dfec71db8012752f2cb39d1d44
Great <jgreat7702gmail.com>
2024-10-07 03:05:01 +0100

    readme

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,9 +5,9 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]  # Start with the first row
-    
+
     for i in range(1, n):
         row = [1]  # First element of each row is always 1
         for j in range(1, i):
@@ -15,5 +15,5 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)  # Last element of each row is always 1
         triangle.append(row)
-    
+
     return triangle
