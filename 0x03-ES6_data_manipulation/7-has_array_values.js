3b18796fa9993ba99c028bcd37d63b895b75be20
Great <jgreat7702gmail.com>
2024-10-08 18:36:42 +0100

    task7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 export default function hasValuesFromArray(aset, arr) {
   for (let i = 0; i < aset.length; i + 1) {
-    if (arr === aset[i]) return true;
+    if (aset.has(arr[i])) return true;
   }
   return false;
 }
